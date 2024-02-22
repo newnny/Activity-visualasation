1861559910f40495d3f14faadaf4712099a5dfa4
Yuni <newnny0812@gmail.com>
2024-02-22 19:26:05 +0100

    modifying dataset

--- components/charts/WorldMapChart.tsx
+++ components/charts/WorldMapChart.tsx
@@ -4,7 +4,7 @@ import { getActivities } from '@/store/stravaAPI/activitiesAPI';
 import { useAppSelector } from '@/store/reduxHooks';
 
 export const WorldMapChart = () => {
-  const activities = useAppSelector(state => state.activities.activities)
+  const activities = useAppSelector(state => state.activities.sorted_activities)
   const loading = useAppSelector(state => state.activities.loading)
 //chart ideas
   //make a category dropdown:run, sailing, etc based on the user activites
